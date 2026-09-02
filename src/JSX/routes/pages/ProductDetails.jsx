01399626c4b87a5e56a7e2b6b692baef2bc25c3b
Shaik-Nazeemuddin <nazeemoddeen@gmail.com>
2026-09-02 11:23:57 +0530

    response json

--- src/JSX/routes/pages/ProductDetails.jsx
+++ src/JSX/routes/pages/ProductDetails.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useNavigate, useParams } from "react-router-dom";
+import { ArrowLeft } from "lucide-react";
 
 const ProductDetails = () => {
   const productDetails = useLoaderData();
@@ -7,20 +8,26 @@ const ProductDetails = () => {
 
   return (
     <div className='custom-component extrapadding'>
-      <h2>Product Details {id}</h2>
+      <h2>Product Details {id && id}</h2>
       <div className="text-left">
-        <p><b>Product Titel :</b>   {productDetails.original_title}</p>
-        <p><b>Product Task :</b>    {productDetails.release_date} </p>
-        <p><b>popularity :</b>      {productDetails.popularity}</p>
-        <p><b>release_date :</b>    {productDetails.release_date}</p>
-        <p><b>vote_average :</b>    {productDetails.vote_average}</p>
-        <p><b>vote_count :</b>      {productDetails.vote_count}</p>
-        <p><b>Product Status :</b>  {productDetails.overview}</p>
+        {productDetails && (
+          <>
+            <p><b>Product Titel :</b>   {productDetails.original_title}</p>
+            <p><b>Product Task :</b>    {productDetails.release_date} </p>
+            <p><b>popularity :</b>      {productDetails.popularity}</p>
+            <p><b>release_date :</b>    {productDetails.release_date}</p>
+            <p><b>vote_average :</b>    {productDetails.vote_average}</p>
+            <p><b>vote_count :</b>      {productDetails.vote_count}</p>
+            <p><b>Product Status :</b>  {productDetails.overview}</p>
+          </>
+         )}
       </div>
-      <button type='button' className="btn btn-info" onClick={() => navigation('/products', { replace: true })}>Back to Products</button>
+      <div className="align-center">
+        <button type='button' className="btn btn-info" onClick={() => navigation('/products', { replace: true })}><ArrowLeft className="margin-right-5" /> Back to Products</button>
+      </div>
     </div>
   )
 }
 
-export default ProductDetails;
+export default  ProductDetails;
 
--- src/JSX/routes/pages/ProductDetailsLoader.jsx
+++ src/JSX/routes/pages/ProductDetailsLoader.jsx
@@ -3,5 +3,5 @@ export const ProductDetailsLoader = async ({ params }) => {
   const { id } = params;
   // const res = await fetch('http://localhost:3000/movies/' + id);
   const res = await fetch('https://node-app-production-8f02.up.railway.app/movies/' + id);
-  return res.json();
+  return await res.json();
 }
